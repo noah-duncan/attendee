5100d415c4f42c20388b96039dde2e1dbcf3375a
Seokhyeon Ryu <199195819+salixnamu@users.noreply.github.com>
2025-04-03 09:40:24 -0700

    remove changelist

--- bots/admin.py
+++ bots/admin.py
@@ -90,6 +90,7 @@ class WebhookDeliveryAdmin(admin.ModelAdmin):
         return '-'
     event_type.short_description = "Bot Event"
 
+    # This doesn't work at the moment, feel free to delete.
     def response_statuses(self, obj):
         if not obj.response_body_list:
             return '-'
@@ -101,42 +102,6 @@ class WebhookDeliveryAdmin(admin.ModelAdmin):
                     status_codes.append(str(status))
         return ', '.join(status_codes) if status_codes else '-'
     response_statuses.short_description = "Response Status Codes"
-
-    def changelist_view(self, request, extra_context=None):
-        # Get successful deliveries in last 30 days
-        from django.utils import timezone
-        from django.db.models import Avg, F, ExpressionWrapper, fields
-        from django.db.models.functions import TruncDay
-
-        thirty_days_ago = timezone.now() - timezone.timedelta(days=30)
-        
-        # Calculate average duration per day
-        duration_data = (
-            self.get_queryset(request)
-            .filter(created_at__gte=thirty_days_ago)
-            .annotate(
-                date=TruncDay('created_at'),
-                duration=ExpressionWrapper(
-                    F('succeeded_at') - F('created_at'),
-                    output_field=fields.DurationField()
-                )
-            )
-            .values('date')
-            .annotate(avg_duration=Avg('duration'))
-            .order_by('date')
-        )
-
-        # Format data for chart
-        dates = [d['date'].strftime('%Y-%m-%d') for d in duration_data]
-        durations = [d['avg_duration'].total_seconds() for d in duration_data]
-
-        extra_context = extra_context or {}
-        extra_context['duration_chart_data'] = {
-            'labels': dates,
-            'data': durations
-        }
-
-        return super().changelist_view(request, extra_context)
 
     list_display = ("idempotency_key", "webhook_url", "status", "created_at", "last_attempt_at", "succeeded_at", "event_type", "response_statuses", "attempt_count", "duration",)
     list_filter = ("status",)
